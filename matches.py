66272f86e6adaaa21ed2363b99a8e7ff4c6aabcf
shakhteremeslo <shakhteremeslo@hotmail.com>
2017-11-18 11:33:43 -0500

    added toScout, now returns matches for prescouting

--- matches.py
+++ matches.py
@@ -15,8 +15,8 @@ def isVideo(matchKey):
     else:
         return True
 
-#
-def isPlayoffs(teamKey, eventKey):
+#determines which matches have video, the types of those matches, and the max match type reached at the event
+def matchLister(teamKey, eventKey):
     matchLookup = json.loads(requests.get("http://www.thebluealliance.com/api/v3/team/" + teamKey + "/event/" + eventKey + "/matches/keys", headers=headers).text)
     #matches is the set of matches a team played at an event which have video
     matches = []
@@ -54,4 +54,43 @@ def isPlayoffs(teamKey, eventKey):
             max_type = "qm"
     return matchdict, max_type
 
-print(isPlayoffs("frc3130","2017mndu2"))
+#determines which matches to prescout
+def toScout(dicttypeset):
+    #takes input from matchLister as list
+    matchdict = dicttypeset[0]
+    max_type = dicttypeset[1]
+    #scoutlist holds matches to prescout
+    scoutlist = []
+    #playoffs is obvious
+    playoffs = False
+    if max_type == "":
+        playoffs = None
+        scoutlist = "No matches found."
+    elif max_type != "qm":
+        playoffs = True
+    #loop picks max playoff match then tries to pick quals, finals, semis, and quarters in that order
+    if playoffs:
+        scoutlist.append(matchdict[max_type].pop(-1))
+        try:
+            try:
+                try:
+                    try:
+                        scoutlist.append(matchdict["qm"][-1])
+                    except:
+                        scoutlist.append(matchdict["f"][-1])
+                except:
+                    scoutlist.append(matchdict["sf"][-1])
+            except:
+                scoutlist.append(matchdict["qf"][-1])
+        except:
+            scoutlist.append("No more matches found.")
+    #loop picks quals
+    elif not playoffs:
+        scoutlist.append(matchdict["qm"].pop(-1))
+        try:
+            scoutlist.append(matchdict["qm"][-1])
+        except:
+            scoutlist.append("No more matches found.")       
+    return scoutlist         
+
+print(toScout(matchLister("frc1257","2017pahat")))
